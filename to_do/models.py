b440b83175adbaea28409981bfeda1d98ad682a5
Cait Powell <caitmpowell@gmail.com>
2023-01-22 17:49:03 +0000

    Complete app setup and create basic initial models

--- to_do/models.py
+++ to_do/models.py
@@ -1,3 +1,20 @@
+from datetime import datetime, timedelta
+
 from django.db import models
 
-# Create your models here.
+
+class ToDoList(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.title
+
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateTimeField(default=(datetime.now() + timedelta(weeks=1)))
+    todo_list = models.ForeignKey(ToDoList, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.title} due on {self.due_date}"
