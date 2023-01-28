d08b5adebdd02168b03c0ee223a2332080f1ec6a
Cait Powell <caitmpowell@gmail.com>
2023-01-28 18:47:51 +0000

    Register models in admin, add template and view for tasks in a list

--- to_do/urls.py
+++ to_do/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("", views.ToDoListOverview.as_view(), name="index"),
+    path("list/<int:list_id>/",
+        views.TaskListView.as_view(), name="list"),
 ]
--- to_do/views.py
+++ to_do/views.py
@@ -1,6 +1,11 @@
 from django.views.generic import ListView
-from .models import ToDoList
+from .models import ToDoList, Task
 
 class ToDoListOverview(ListView):
     model = ToDoList
     template_name = "to_do/index.html"
+
+class TaskListView(ListView):
+    model = Task
+    template_name = "to_do/todo_list.html"
+
