1f00fb411c06df8d60d044cc2dae377e27e19396
Cait Powell <caitmpowell@gmail.com>
2023-01-28 18:49:58 +0000

    Add todo_list to tasklistview context for display

--- to_do/views.py
+++ to_do/views.py
@@ -9,3 +9,7 @@ class TaskListView(ListView):
     model = Task
     template_name = "to_do/todo_list.html"
 
+    def get_context_data(self):
+            context = super().get_context_data()
+            context["todo_list"] = ToDoList.objects.get(id=self.kwargs["list_id"])
+            return context
